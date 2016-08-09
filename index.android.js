af9afa0ff7d635a71239ce3afc0031b322d88476
desiter <m.nawrot@descript.org>
2016-08-09 17:23:10 +0200

    Android project

---
+++ index.android.js
@@ -0,0 +1,11 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+import React, { AppRegistry } from 'react-native';
+import App from './app';
+
+AppRegistry.registerComponent('reactnative_redux_listview', () => App);
